0752e2b076a1fa2dcf867ec3c273da0bda28320f
Esther Olatunde <olatundeesther@gmail.com>
2017-11-28 18:30:01 +0100

    Rewrites template in html instead of pug so as to gain a better understanding of the layout

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,13 +1,25 @@
-<template lang="pug">
-  body
-    .notes.row
-      .column.column-20.sidebar
-        h5
-          a(href='#' @click="addNote") +
-        ul
-          li(v-for="note in notes" :key="note.id" :class='{ active: note === selected }' @click="selectNote(note)") {{ note.body }}
-      .column.column-80(style='position: relative')
-        textarea(v-if='selected' v-model="selected.body" placeholder="Edit me")
+<template>
+  <body>
+    <div class="notes">
+      <div class="row">
+          <div class="column column-20 sidebar">
+          <h5>
+            <a href="#" v-on:click="addNote"> + </a>
+            <ul>
+              <li v-for="note in notes" :key="note.id" class="{ active: note=== selected}" v-on:click="selectNote(note)">
+                {{ note.body }}
+              </li>
+            </ul>
+          </h5>
+          </div>
+          <div class="column column-80" style="position:relative;">
+            <textarea v-if="selected" v-model="selected.body" placeholder="Edit me">
+            </textarea>
+          </div>        
+      </div>
+      
+    </div>
+  </body>
 </template>
 
 <script>
