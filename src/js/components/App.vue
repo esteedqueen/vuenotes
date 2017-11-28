61aecf51df78daffb07f6fb81db943d29546bc91
Esther Olatunde <olatundeesther@gmail.com>
2017-11-28 18:16:17 +0100

    Adds component and functions for adding notes  Adds milligram plugin for minimalist styles

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,13 +1,51 @@
-<template>
-  <h1>Hello World</h1>
+<template lang="pug">
+  body
+    .notes.row
+      .column.column-20.sidebar
+        h5
+          a(href='#' @click="addNote") +
+        ul
+          li(v-for="note in notes" :key="note.id" :class='{ active: note === selected }' @click="selectNote(note)") {{ note.body }}
+      .column.column-80(style='position: relative')
+        textarea(v-if='selected' v-model="selected.body" placeholder="Edit me")
 </template>
 
 <script>
+// requiring milligram framework styles
+require('milligram');
+
 export default {
-  // we'll add stuff later
-  data: function() {
-    return {};
-  }
+  data: () => {
+    return {
+      notes: [],
+      selected: undefined
+    };
+  },
+  methods: { addNote, selectNote }
+};
+
+
+function selectNote(note) {
+  if (note === this.selected) return;
+  this.selected = note;
+}
+
+function addNote() {
+  const note = { id: guid(), body: '# ' };
+  this.notes.unshift(note);
+  this.selectNote(note);
+}
+
+// generate unique IDs
+function guid () {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
 }
 </script>
 
